1ef3f7d8140d77f76fc707cc85c49a2b2016eee4
Hammad Naeem <hammaddotio@gmail.com>
2024-11-14 22:44:55 +0500

    react-native stater project with tailwindcss

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import {
+  SafeAreaView,
+  View,
+  Text
+} from 'react-native'
+
+import './global.css'
+const App = () => {
+  return (
+    <SafeAreaView className="flex-1 bg-black">
+      <View className="flex-1 justify-center">
+        <Text className="text-white text-center text-2xl">hMM</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App
